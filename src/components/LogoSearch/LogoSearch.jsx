aa23589320c5c9e1dfbdd75f5e85e87363b4f4d4
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-24 18:18:51 +0300

    LogoSearch Added

--- src/components/LogoSearch/LogoSearch.jsx
+++ src/components/LogoSearch/LogoSearch.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Logo from '../../img/logo.png'
 import {UilSearch} from '@iconscout/react-unicons'
+import './LogoSearch.css'
 const LogoSearch = () => {
   return (
     <div className="LogoSearch">
