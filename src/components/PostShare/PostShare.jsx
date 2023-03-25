3571cf6bb5c01b310390223b436a6a40e85e528d
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-25 18:33:47 +0300

    Added Post Sharing Section

---
+++ src/components/PostShare/PostShare.jsx
@@ -0,0 +1,70 @@
+import React,{useState, useRef} from 'react';
+import Profile from '../../img/profileImg.jpeg';
+import "./PostShare.css";
+import { UilScenery } from "@iconscout/react-unicons";
+import { UilPlayCircle } from "@iconscout/react-unicons";
+import { UilLocationPoint } from "@iconscout/react-unicons";
+import { UilSchedule } from "@iconscout/react-unicons";
+import { UilTimes } from "@iconscout/react-unicons";
+
+const PostShare = () => {
+  const [image, setImage] = useState(null)
+  const imageRef = useRef()
+
+  const onImageChange =(event)=>{
+    if(event.target.files && event.target.files[0]) {
+        let img = event.target.files[0];
+        setImage({
+            image: URL.createObjectURL(img),
+        });
+    }
+  }
+  return (
+    <div className="PostShare">
+        <img src={Profile} alt="" />
+        <div>
+         <input type="text" placeholder="What's happening" />
+        <div className="postOptions">
+            <div className="option"
+            style={{color: "var(--photo)"}}
+            onClick={()=>imageRef.current.click()}
+            >
+                <UilScenery/>
+                Photo
+            </div>
+            <div className="option"
+            style={{color: "var(--video)"}}
+            >
+                <UilPlayCircle/>
+                Video
+            </div> {""}
+            <div className="option"
+            style={{color: "var(--location)"}}
+            >
+                <UilLocationPoint/>
+                Location
+            </div>{""}
+            <div className="option"
+            style={{color: "var(--schedule)"}}
+            >
+                <UilSchedule/>
+                Schedule
+                </div>
+                <button className="button ps-button">Share</button>
+                <div style={{display: "none"}}>
+                    <input type="file" name="myImage" ref={imageRef} onChange={onImageChange} />
+                </div>
+            </div>
+            {image && (
+
+                <div className="previewImage">
+                    <UilTimes onClick={()=>setImage(null)}/>
+                    <img src={image.image} alt="" />
+                </div>
+            )}
+        </div>
+    </div>
+  );
+};
+
+export default PostShare;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import PostSide from "../../components/PostSide/PostSide.jsx"
 import ProfileSide from "../../components/profileSide/ProfileSide.jsx";
 import "./Home.css"
 const Home = () => {
   return (
     <div className="Home">
         <ProfileSide />
-        <div className="postSide">Posts</div>
+        <PostSide />
         <div className="RightSide">Rightside</div>
     </div>
   )
