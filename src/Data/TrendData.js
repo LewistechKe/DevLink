92aa6a9d4f39c228960e3d77f93adcdafa0ef9c1
Lewis Muthaura <lewislemmu10@gmail.com>
2023-03-26 17:25:21 +0300

    Trending Section

---
+++ src/Data/TrendData.js
@@ -0,0 +1,26 @@
+export const TrendData = [
+    {
+        name: "Minions",
+        shares: 75.5,
+    },
+    {
+        name: "Avengers",
+        shares: 80.5,
+    },
+    {
+        name: "Lewistech.co.ke",
+        shares: 97.5,
+    },
+    {
+        name: "Reactjs",
+        shares: 72,
+    },
+    {
+        name: "Elon Musk",
+        shares: 71.9,
+    },
+    {
+        name: "Need for Speed",
+        shares: 20,
+    },
+]
--- src/components/TrendCard/TrendCard.jsx
+++ src/components/TrendCard/TrendCard.jsx
@@ -6,7 +6,14 @@ const TrendCard = () => {
   return (
     <div className="TrendCard">
             <h3>Trends for you</h3>
-            {}
+            {TrendData.map((trend)=>{
+                return(
+                   <div className="trend">
+                    <span>#{trend.name}</span>
+                    <span>{trend.shares}k shares</span>
+                   </div> 
+                )
+            })}
 
 
     </div>
